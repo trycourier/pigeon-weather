df293271eb2d487eace88100588e207bdc62dc72
Glitch (pigeon-weather) <none>
2020-03-17 13:34:39 +0000

    😀🎊 Updated with Glitch 03/11/2020 Courier Live

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const UsersRoute = require("./users");
+const { User } = require("../models");
+const { CourierClient } = require("@trycourier/courier");
+const getCurrentWeather = require("../lib/getCurrentWeather");
+
+const courier = CourierClient();
 
 router.use("/Users", UsersRoute);
+
+router.post("/send", async (req, res) => {
+  const { userIds } = req.body;
+  for (let id of userIds) {
+    const user = await User.findOne({ where: { id } });
+    const weather = await getCurrentWeather(user.zipCode, user.tempScale);
+    const { messageId } = await courier.send({
+      eventId: "CURRENT_FORECAST",
+      recipientId: id,
+      data: {
+        weather
+      }
+    });
+  }
+  res.sendStatus(202);
+});
 
 module.exports = router;
